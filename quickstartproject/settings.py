ba7d4095e1fd8a79e33109655d7f2d44edf80542
SirraSneha18 <156457190+SirraSneha18@users.noreply.github.com>
2025-02-08 11:24:49 +0530

    Update settings.py

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vhfzje25wg8&lk#tqmo%%^junr4f82f(*h-@2h6%$h%cl7-ao_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
